d5c0ad661e789c536f33088123fe3b66d82507ef
kumarsatyasai <tulasikumarkande@gmail.com>
2025-07-21 14:48:23 +0530

    Initial commit layered application one

---
+++ src/main/java/com/nt/LayeredApplication2.java
@@ -0,0 +1,33 @@
+package com.nt;
+
+import java.util.List; 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import com.nt.controller.EmployeeController;
+import com.nt.model.Employee; 
+@SpringBootApplication
+public class LayeredApplication2 {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx=SpringApplication.run(LayeredApplication2.class, args);
+		EmployeeController employeeController = ctx.getBean("empController", EmployeeController.class);
+		
+		try
+		{
+			List<Employee> list = employeeController.showByEmployeesJobs("CLERK", "MANAGER", "SALESMAN");
+			
+			list.forEach(emp->System.out.println(emp));
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
